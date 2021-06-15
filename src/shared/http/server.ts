e471e5bb4bb482fd2346326ed5e0c71d5a0ac212
Guilherme Pupo <guilh.pupo@gmail.com>
2021-06-15 15:30:39 -0300

    aula 15

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
@@ -8,6 +9,17 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError)
+    return res
+      .status(err.statusCode)
+      .json({ status: 'error', message: err.message });
+
+  return res
+    .status(500)
+    .json({ status: 'error', message: 'Internal server error' });
+});
 
 app.listen(3333, () => {
   console.log('Server started on port 3333! 🏆');
