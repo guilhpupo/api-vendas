5f4f5d90949c2a51d73c09ee134c1e6aec1e643f
Guilherme Pupo <guilh.pupo@gmail.com>
2021-06-17 15:16:45 -0300

    aula 26

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,34 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import { ProductsRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class CreateProductService {
+  public async execute({ name, price, quantity }: IRequest): Promise<Product> {
+    const productRepository = getCustomRepository(ProductsRepository);
+
+    const alreadyExists = await productRepository.findByName(name);
+
+    if (alreadyExists) {
+      throw new AppError('This product name already exists! Try another name');
+    }
+
+    const product = productRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await productRepository.save(product);
+
+    return product;
+  }
+}
+
+export default CreateProductService;
