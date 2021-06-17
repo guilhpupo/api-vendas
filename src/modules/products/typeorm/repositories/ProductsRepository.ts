48e2fc7701d2b6f9d8e5beb9cddbccc4910cf0f8
Guilherme Pupo <guilh.pupo@gmail.com>
2021-06-17 14:56:49 -0300

    aula 25

---
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -0,0 +1,15 @@
+import { EntityRepository, Repository } from 'typeorm';
+import Product from '../entities/Product';
+
+@EntityRepository()
+export class ProductsRepository extends Repository<Product> {
+  public async findByName(name: string): Promise<Product | undefined> {
+    const product = await this.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return product;
+  }
+}
