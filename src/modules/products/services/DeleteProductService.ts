d3ddccbde75dfb4c5be2453b7cf71c7f4d199e09
Guilherme Pupo <guilh.pupo@gmail.com>
2021-06-17 16:38:24 -0300

    aula 31

---
+++ src/modules/products/services/DeleteProductService.ts
@@ -0,0 +1,22 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { ProductsRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  id: string;
+}
+class DeleteProductService {
+  public async execute({ id }: IRequest): Promise<void> {
+    const productRepository = getCustomRepository(ProductsRepository);
+
+    const product = await productRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found', 404);
+    }
+
+    await productRepository.remove(product);
+  }
+}
+
+export default DeleteProductService;
