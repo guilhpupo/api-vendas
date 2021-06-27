e47733018163cd3767e4b22431754133b22dee11
Guilherme Pupo <guilh.pupo@gmail.com>
2021-06-27 04:06:53 -0300

    aula 40

---
+++ src/modules/users/controllers/UsersController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import CreateUserService from '../services/CreateUserService';
+import ListUsersService from '../services/ListUsersService';
+
+class UsersController {
+  public async index(req: Request, res: Response): Promise<Response> {
+    const listUsers = new ListUsersService();
+
+    const users = await listUsers.execute();
+
+    return res.json(users);
+  }
+  public async store(req: Request, res: Response): Promise<Response> {
+    const { name, email, password } = req.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    return res.json(user);
+  }
+}
+
+export default UsersController;
